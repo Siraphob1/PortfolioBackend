8f7b38ab24e103d084a979664aa2252fcd52e4ba
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-05 04:05:17 +0700

    create server

---
+++ server.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3600 
+const cors = require('cors')
+const nodemailer = require("nodemailer");
+const sendemail = require('./model/sendemail');
+const helmet = require('helmet');
+
+//middleware 3-party
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(helmet());
+app.disable('x-powered-by');
+
+
+
+// console.log(process.env.ESENDER)
+app.post('/sendmail' , async (req , res)=>{
+    const {namevisitor , emailvisitor , messagevisitor} = req.body;   
+
+    if(!namevisitor || !emailvisitor || !messagevisitor) return res.sendStatus(400)
+
+    const reciever_email = 'siraphobtop95@gmail.com';
+    const sender_email = process.env.ESENDER;
+    const sender_pwd = process.env.PSENDER;
+
+        //Create tranpoter
+        const transporter = nodemailer.createTransport({
+            service: 'hotmail',
+            auth: {
+              user: sender_email,
+              pass: sender_pwd
+            }
+          });
+    
+        //Setup message options
+        const  mailOptions = {
+            from: sender_email,
+            to: reciever_email,
+            subject: 'Portfolio Siraphob',
+            html:sendemail(namevisitor , emailvisitor , messagevisitor)
+        }
+    
+         //SendMail
+         await transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+              res.status(503).json({'sendestatus': 'failed'});
+            } else {
+              console.log('Email sent: ' + info.response);    
+              res.status(200).json({message:'success'})
+            }
+          });
+    
+    
+})
+
+app.listen(port , ()=>{
+    console.log(`Run server at http://localhost:${port}/`)
+})
